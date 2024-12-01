22fa6518b2b5320af2862ccbfa5eb2c814d7dbd3
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-12-02 00:35:25 +0300

    feat: added additional transparencies

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -7,6 +7,7 @@ import backend.academy.fractal.flame.ImageUtils;
 import backend.academy.fractal.flame.Point;
 import backend.academy.fractal.flame.Rect;
 import backend.academy.fractal.flame.Transformation;
+import backend.academy.fractal.flame.TransformationImpl;
 import lombok.experimental.UtilityClass;
 import java.nio.file.Path;
 import java.util.List;
@@ -24,15 +25,13 @@ public class Main {
 
         FractalImage canvas = FractalImage.create(width, height);
 
-        List<Transformation> transformations = List.of(
-                point -> new Point(Math.sin(point.getX()) * point.getY(), Math.sin(point.getY()) * point.getX()),
-                point -> new Point(point.getX() * Math.sin(point.getY()), point.getY() * Math.sin(point.getX())),
-                point -> new Point(point.getX() / (point.getX() * point.getX() + point.getY() * point.getY()),
-                        point.getY() / (point.getX() * point.getX() + point.getY() * point.getY())),
-                point -> new Point(Math.sin(point.getX() * point.getX() - point.getY() * point.getY()),
-                        Math.sin(2 * point.getX() * point.getY())),
-                point -> new Point(Math.tan(point.getX()), Math.tan(point.getY())),
-                point -> new Point(point.getX() * Math.cos(point.getY()), point.getY() * Math.sin(point.getX()))
+        var transformations = List.of(
+                TransformationImpl.custom1(),
+                TransformationImpl.custom2(),
+                TransformationImpl.spherical(),
+                TransformationImpl.custom3(),
+                TransformationImpl.tangent(),
+                TransformationImpl.custom4()
         );
 
 
--- src/main/java/backend/academy/fractal/flame/TransformationImpl.java
+++ src/main/java/backend/academy/fractal/flame/TransformationImpl.java
@@ -2,7 +2,6 @@ package backend.academy.fractal.flame;
 
 public class TransformationImpl implements Transformation {
     public Point apply(Point p) {
-        // Базовая трансформация, можно использовать как linear
         return p;
     }
 
@@ -56,5 +55,59 @@ public class TransformationImpl implements Transformation {
             }
         };
     }
-}
 
+    public static Transformation custom1() {
+        return new Transformation() {
+            @Override
+            public Point apply(Point p) {
+                double x = Math.sin(p.getX()) * p.getY();
+                double y = Math.sin(p.getY()) * p.getX();
+                return new Point(x, y);
+            }
+        };
+    }
+
+    public static Transformation custom2() {
+        return new Transformation() {
+            @Override
+            public Point apply(Point p) {
+                double x = p.getX() * Math.sin(p.getY());
+                double y = p.getY() * Math.sin(p.getX());
+                return new Point(x, y);
+            }
+        };
+    }
+
+    public static Transformation custom3() {
+        return new Transformation() {
+            @Override
+            public Point apply(Point p) {
+                double x = Math.sin(p.getX() * p.getX() - p.getY() * p.getY());
+                double y = Math.sin(2 * p.getX() * p.getY());
+                return new Point(x, y);
+            }
+        };
+    }
+
+    public static Transformation tangent() {
+        return new Transformation() {
+            @Override
+            public Point apply(Point p) {
+                double x = Math.tan(p.getX());
+                double y = Math.tan(p.getY());
+                return new Point(x, y);
+            }
+        };
+    }
+
+    public static Transformation custom4() {
+        return new Transformation() {
+            @Override
+            public Point apply(Point p) {
+                double x = p.getX() * Math.cos(p.getY());
+                double y = p.getY() * Math.sin(p.getX());
+                return new Point(x, y);
+            }
+        };
+    }
+}
